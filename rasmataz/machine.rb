26e326a4c20aa895445b16b9dd7bc66fe25cd5a9
James Ladd <james_ladd@hotmail.com>
2010-12-01 16:20:06 +1100

    Executing instructions.

--- rasmataz/machine.rb
+++ rasmataz/machine.rb
@@ -13,6 +13,8 @@ module RASMATAZ
 
       def instructions
         <<-INSTRUCTIONS
+        halt
+        nop
         mov src dst
         push src
         pop dst
@@ -20,6 +22,14 @@ module RASMATAZ
         jnz lbl
         label name
         INSTRUCTIONS
+      end
+
+      def halt()
+        encode instruction.with(:mnemonic => :halt)
+      end
+
+      def nop()
+        encode instruction.with(:mnemonic => :nop)
       end
 
       def mov(src, dst)
@@ -47,7 +57,12 @@ module RASMATAZ
       end
  
       def go
-        raise "No :start label found. Define one with 'label :start'." if labels[:start].nil?
+        raise "No #{start_label} label found. Define one with 'label #{start_label}'." if labels[start_label].nil?
+		initialize_execution_registers
+        while not memory[instruction_pointer].nil?
+          execute memory[instruction_pointer]
+          increment_instruction_pointer
+        end
       end
 
     private 
@@ -59,16 +74,45 @@ module RASMATAZ
         @registers = hash_of_all_register_names
       end
 
+      def initialize_execution_registers
+        registers[stack_pointer_register] = stack.size
+        registers[instruction_pointer_register] = labels[start_label]
+      end
+
+      def start_label
+        :start
+      end
+
+      def stack_pointer_register
+        :rsp
+      end
+
+      def instruction_pointer_register
+        :rip
+      end
+
+      def instruction_pointer
+        registers[instruction_pointer_register]
+      end
+
+      def increment_instruction_pointer
+        registers[instruction_pointer_register] += 1
+      end
+
+      def execute(instruction)
+        puts instruction.inspect
+      end
+ 
       def encode(instruction)
         # empty - here to help readability of adding instructions for execution.
       end
 
       def encode_label(instruction)
-        @labels[instruction.mnemonic] = instruction.pointer
+        @labels[instruction.arg1] = instruction.pointer
       end
 
       def instruction
-        @memory << Instruction.new(@memory.size)
+        @memory << Instruction.new(self, @memory.size)
         @memory.last
       end
 
@@ -80,11 +124,11 @@ module RASMATAZ
       end
 
       def general_register_names
-        [ :rax, :rbx, :rcx, :rdx ]
+        [:rax, :rbx, :rcx, :rdx]
       end
 
       def index_and_pointer_register_names
-        [ :rsi, :rdi, :rbp, :rip, :rsp ]
+        [:rsi, :rdi, :rbp, instruction_pointer_register, stack_pointer_register]
       end
   end
 
@@ -92,7 +136,7 @@ module RASMATAZ
 
     attr_accessor :mnemonic, :pointer, :arg1, :arg2
 
-    def initialize(pointer)
+    def initialize(machine, pointer)
       @pointer = pointer
     end
 
